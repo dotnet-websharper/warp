f2cadf243ca2b54b8cbb50ae35175822cc37b043
Paul Blair <paulblair@Pauls-Mac-mini.home>
2015-08-12 09:01:42 -0400

    Use OWIN logger.

--- WebSharper.Warp.Test/LoggingMiddleware.fs
+++ WebSharper.Warp.Test/LoggingMiddleware.fs
@@ -5,7 +5,9 @@ open System.Collections.Generic
 open System.Threading.Tasks
 open System.IO
 
+open Owin
 open Microsoft.Owin
+open Microsoft.Owin.Logging
 open WebSharper.Owin
 
 module Logging = 
@@ -17,66 +19,72 @@ module Logging =
     let dictionaryRequestKeys =  ["owin.RequestHeaders"; "Microsoft.Owin.Cookies#dictionary"; "Microsoft.Owin.Query#dictionary"]
 
     let simpleResponseKeys =     ["owin.ResponseStatusCode"; "owin.RequestMethod"; "owin.RequestPath"]
-    let dictionaryResponseKeys = ["owin.ResponseHeaders"]    
+    let dictionaryResponseKeys = ["owin.ResponseHeaders"]   
 
-    let logEnvValue (dict: IDictionary<string,'a>) key = 
-        match dict.TryGetValue(key) with
-        | (true, value) -> Console.WriteLine("{0}: {1}", key, value)
-        | _ -> () 
+    type OwinLogger(next: AppFunc, appBuilder: Owin.IAppBuilder) =
 
-    let logDictValues (env: Env) key = 
-        match env.TryGetValue(key) with
-        | (true, (:? IDictionary<string, string> as dictionary)) -> dictionary.Keys |> Seq.iter (logEnvValue dictionary)
-        | _ -> () 
+        let logEnvValue (logger: ILogger) (dict: IDictionary<string,'a>) key = 
+            match dict.TryGetValue(key) with
+            | (true, value) -> logger.WriteInformation(String.Format("{0}: {1}", key, value))
+            | _ -> () 
 
-    let logRequest env = 
-        Console.WriteLine(String.Format("{0}REQUEST:", Environment.NewLine))
-        simpleRequestKeys |> Seq.iter (logEnvValue env)
-        dictionaryRequestKeys |> Seq.iter (logDictValues env)
+        let logDictValues (logger: ILogger) (env: Env) key = 
+            match env.TryGetValue(key) with
+            | (true, (:? IDictionary<string, string> as dictionary)) -> dictionary.Keys |> Seq.iter (logEnvValue logger dictionary)
+            | _ -> () 
 
-    let logResponse env =
-        Console.WriteLine(String.Format("{0}RESPONSE:", Environment.NewLine))
-        simpleResponseKeys |> Seq.iter (logEnvValue env)
-        dictionaryResponseKeys |> Seq.iter (logDictValues env)
+        let logRequest (logger: ILogger) env = 
+            Console.WriteLine("Logging REQUEST: - Console")
+            logger.WriteInformation(String.Format("{0}REQUEST:", Environment.NewLine))
+            simpleRequestKeys |> Seq.iter (logEnvValue logger env)
+            dictionaryRequestKeys |> Seq.iter (logDictValues logger env)
+            Console.WriteLine("End Logging REQUEST: - Console")
 
-    let toMiddlewareGenerator (middlewareFunc: AppFunc -> Env -> Task): MiddlewareGenerator =
-        MiddlewareGenerator(fun (appBuilder: Owin.IAppBuilder) -> MidFunc(fun next -> AppFunc(middlewareFunc next)))
+        let logResponse (logger: ILogger) env =
+            logger.WriteInformation(String.Format("{0}RESPONSE:", Environment.NewLine))
+            simpleResponseKeys |> Seq.iter (logEnvValue logger env)
+            dictionaryResponseKeys |> Seq.iter (logDictValues logger env)
 
-    let logger: MiddlewareGenerator =
-        let loggerFunc (next: AppFunc) (env: Env) = 
+        member this.Invoke(env: Env) =               
             async {
+                let logger = appBuilder.CreateLogger<OwinLogger>()
                 let context = new OwinContext(env)
                 let responseStream = context.Response.Body
                 let responseBuffer = new MemoryStream() :> Stream
 
-                logRequest env
+                logRequest logger env
 
-                if logRequestBody then
+                if logger.IsEnabled(Diagnostics.TraceEventType.Verbose) then
                     let requestStream = context.Request.Body
                     let buffer = new MemoryStream()
                     do! requestStream.CopyToAsync(buffer) |> Async.AwaitIAsyncResult |> Async.Ignore
                     let inputStreamReader = new StreamReader(buffer)
                     let! requestBody = inputStreamReader.ReadToEndAsync() |> Async.AwaitTask
-                    Console.WriteLine("Request Body:")
-                    Console.WriteLine(requestBody)
+                    logger.WriteVerbose("Request Body:")
+                    logger.WriteVerbose(requestBody)
                     buffer.Seek(0L, SeekOrigin.Begin) |> ignore
                     do! buffer.CopyToAsync(requestStream) |> Async.AwaitIAsyncResult |> Async.Ignore
-
-                if logResponseBody then
                     context.Response.Body <- responseBuffer
 
                 do! next.Invoke(env) |> Async.AwaitIAsyncResult |> Async.Ignore
 
-                logResponse env
+                logResponse logger env
 
-                if logResponseBody then
+                if logger.IsEnabled(Diagnostics.TraceEventType.Verbose) then
                     responseBuffer.Seek(0L, SeekOrigin.Begin) |> ignore
                     let outputStreamReader = new StreamReader(responseBuffer)
                     let! responseBody = outputStreamReader.ReadToEndAsync() |> Async.AwaitTask
-                    Console.WriteLine("Response Body:")
-                    Console.WriteLine(responseBody)
+                    logger.WriteVerbose("Response Body:")
+                    logger.WriteVerbose(responseBody)
                     responseBuffer.Seek(0L, SeekOrigin.Begin) |> ignore
                     do! responseBuffer.CopyToAsync(responseStream) |> Async.AwaitIAsyncResult |> Async.Ignore
 
             } |> Async.Ignore |> Async.StartAsTask :> Task
-        loggerFunc |> toMiddlewareGenerator
+
+    let logger: MiddlewareGenerator = 
+        let loggerFunc (appBuilder: Owin.IAppBuilder) =
+            let midFunc(next: AppFunc) = 
+                let appFunc (env: Env) = (new OwinLogger(next, appBuilder)).Invoke(env)
+                AppFunc(appFunc)
+            MidFunc(midFunc)
+        MiddlewareGenerator(loggerFunc)
