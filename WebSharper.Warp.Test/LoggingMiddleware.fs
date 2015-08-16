512d6e4c47750c58f7d938f99db6944164b2ac44
Paul Blair <psfblair@gmail.com>
2015-08-16 12:15:57 -0400

    More refinements on Logging middleware.

--- WebSharper.Warp.Test/LoggingMiddleware.fs
+++ WebSharper.Warp.Test/LoggingMiddleware.fs
@@ -8,41 +8,66 @@ open System.IO
 open Owin
 open Microsoft.Owin
 open Microsoft.Owin.Logging
+open Microsoft.Owin.Host.HttpListener.RequestProcessing
 open WebSharper.Owin
+open WebSharper
 
 module Logging = 
-    let logRequestBody = false
-    let logResponseBody = false
-
-    let simpleRequestKeys = [ "owin.RequestScheme";  "owin.RequestProtocol";  "owin.RequestMethod";  "owin.RequestPathBase";  "owin.RequestPath";    
-                              "owin.RequestQueryString"; "server.RemoteIpAddress"; "server.RemotePort";  "server.LocalIpAddress";  "server.LocalPort"]
-    let dictionaryRequestKeys =  ["owin.RequestHeaders"; "Microsoft.Owin.Cookies#dictionary"; "Microsoft.Owin.Query#dictionary"]
-
-    let simpleResponseKeys =     ["owin.ResponseStatusCode"; "owin.RequestMethod"; "owin.RequestPath"]
-    let dictionaryResponseKeys = ["owin.ResponseHeaders"]   
 
     type OwinLogger(next: AppFunc, appBuilder: Owin.IAppBuilder) =
 
-        let logEnvValue (logger: ILogger) (dict: IDictionary<string,'a>) key = 
-            match dict.TryGetValue(key) with
-            | (true, value) -> logger.WriteInformation(String.Format("{0}: {1}", key, value))
-            | _ -> () 
+        let logKeyValuePair (logger: ILogger) (pair: KeyValuePair<string, string>) = 
+            logger.WriteInformation(String.Format("\t{0}: {1}", pair.Key, pair.Value))
+        
+        let convertArrayValues (pair: KeyValuePair<string, array<string>>) =
+            new KeyValuePair<string, string>(pair.Key, String.Join(",", pair.Value))
 
-        let logDictValues (logger: ILogger) (env: Env) key = 
-            match env.TryGetValue(key) with
-            | (true, (:? IDictionary<string, string> as dictionary)) -> dictionary.Keys |> Seq.iter (logEnvValue logger dictionary)
-            | _ -> () 
+        let logDictValues (logger: ILogger) (env: Env) (envKey: string) = 
+            logger.WriteVerbose(String.Format("{0}:", envKey))
+            match env.TryGetValue(envKey) with
+            | (true, (:? IDictionary<string, array<string>> as dictionary)) -> dictionary 
+                                                                                |> Seq.map convertArrayValues  
+                                                                                |> Seq.iter (logKeyValuePair logger)
+            | (true, (:? IDictionary<string, string> as dictionary)) -> dictionary 
+                                                                                |> Seq.iter (logKeyValuePair logger)
+            | _ -> ()
+        
+        let defaultValue value result = 
+            match result with
+            | (true, resultValue) -> resultValue.ToString()
+            | _ -> value 
+
+        let requestToString time (dict: IDictionary<string,'a>) = 
+            let remoteIpAddress    = dict.TryGetValue("server.RemoteIpAddress")  |> defaultValue ""
+            let remotePort         = dict.TryGetValue("server.RemotePort")       |> defaultValue ""
+            let protocol           = dict.TryGetValue("owin.RequestProtocol")    |> defaultValue ""
+            let localIP            = dict.TryGetValue("server.LocalIpAddress")   |> defaultValue ""
+            let localPort          = dict.TryGetValue("server.LocalPort")        |> defaultValue ""
+            let requestMethod      = dict.TryGetValue("owin.RequestMethod")      |> defaultValue ""
+            let requestPath        = dict.TryGetValue("owin.RequestPath")        |> defaultValue ""
+            let requestQueryString = dict.TryGetValue("owin.RequestQueryString") |> defaultValue ""
+
+            String.Format("{0} {1}:{2} {3} {4} {5} {6} from {7}:{8}", 
+                            time, localIP, localPort, requestMethod, requestPath, requestQueryString, 
+                            protocol, remoteIpAddress, remotePort ) 
 
         let logRequest (logger: ILogger) env = 
-            logger.WriteInformation(String.Format("{0}REQUEST:", Environment.NewLine))
-            simpleRequestKeys |> Seq.iter (logEnvValue logger env)
-            dictionaryRequestKeys |> Seq.iter (logDictValues logger env)
+            logger.WriteVerbose("Request:")
+            requestToString DateTime.Now env |> logger.WriteInformation
+            logDictValues logger env "owin.RequestHeaders"
+
+        let responseToString time (dict: IDictionary<string,'a>) = 
+            let requestMethod   = dict.TryGetValue("owin.RequestMethod")      |> defaultValue ""
+            let requestPath     = dict.TryGetValue("owin.RequestPath")        |> defaultValue ""
+            let responseStatus  = dict.TryGetValue("owin.ResponseStatusCode") |> defaultValue ""
+
+            String.Format("{0} STATUS {1} for {2} {3}", time, responseStatus, requestMethod, requestPath)
 
         let logResponse (logger: ILogger) env =
-            logger.WriteInformation(String.Format("{0}RESPONSE:", Environment.NewLine))
-            simpleResponseKeys |> Seq.iter (logEnvValue logger env)
-            dictionaryResponseKeys |> Seq.iter (logDictValues logger env)
-
+            logger.WriteVerbose("Response:")
+            responseToString DateTime.Now env |> logger.WriteInformation
+            logDictValues logger env "owin.ResponseHeaders"
+        
         member this.Invoke(env: Env) =               
             async {
                 let logger = appBuilder.CreateLogger<OwinLogger>()
@@ -54,14 +79,15 @@ module Logging =
 
                 if logger.IsEnabled(Diagnostics.TraceEventType.Verbose) then
                     let requestStream = context.Request.Body
-                    let buffer = new MemoryStream()
-                    do! requestStream.CopyToAsync(buffer) |> Async.AwaitIAsyncResult |> Async.Ignore
-                    let inputStreamReader = new StreamReader(buffer)
+                    let requestBuffer = new MemoryStream()
+                    do! requestStream.CopyToAsync(requestBuffer) |> Async.AwaitIAsyncResult |> Async.Ignore
+                    requestBuffer.Seek(0L, SeekOrigin.Begin) |> ignore
+                    let inputStreamReader = new StreamReader(requestBuffer)
                     let! requestBody = inputStreamReader.ReadToEndAsync() |> Async.AwaitTask
                     logger.WriteVerbose("Request Body:")
                     logger.WriteVerbose(requestBody)
-                    buffer.Seek(0L, SeekOrigin.Begin) |> ignore
-                    do! buffer.CopyToAsync(requestStream) |> Async.AwaitIAsyncResult |> Async.Ignore
+                    requestBuffer.Seek(0L, SeekOrigin.Begin) |> ignore
+                    context.Request.Body <- requestBuffer
                     context.Response.Body <- responseBuffer
 
                 do! next.Invoke(env) |> Async.AwaitIAsyncResult |> Async.Ignore
@@ -77,7 +103,8 @@ module Logging =
                     responseBuffer.Seek(0L, SeekOrigin.Begin) |> ignore
                     do! responseBuffer.CopyToAsync(responseStream) |> Async.AwaitIAsyncResult |> Async.Ignore
 
-            } |> Async.Ignore |> Async.StartAsTask :> Task
+            } |> Async.Ignore 
+              |> Async.StartAsTask :> Task
 
     let logger: MiddlewareGenerator = 
         let loggerFunc (appBuilder: Owin.IAppBuilder) =
@@ -86,3 +113,4 @@ module Logging =
                 AppFunc(appFunc)
             MidFunc(midFunc)
         MiddlewareGenerator(loggerFunc)
+
